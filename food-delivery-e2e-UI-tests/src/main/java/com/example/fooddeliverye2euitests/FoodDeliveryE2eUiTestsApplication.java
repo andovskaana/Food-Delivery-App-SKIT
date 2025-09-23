b52857b8b7dfc69aa3898f7e82e5bf6ffc52049b
andovskaana <138378875+andovskaana@users.noreply.github.com>
2025-09-23 19:07:35 +0200

    Adding folder for ui testing
    .

---
+++ food-delivery-e2e-UI-tests/src/main/java/com/example/fooddeliverye2euitests/FoodDeliveryE2eUiTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fooddeliverye2euitests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryE2eUiTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryE2eUiTestsApplication.class, args);
+    }
+
+}
---
+++ food-delivery-e2e-UI-tests/src/test/java/com/example/fooddeliverye2euitests/FoodDeliveryE2eUiTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fooddeliverye2euitests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryE2eUiTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
